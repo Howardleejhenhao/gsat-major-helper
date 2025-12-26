5d1d944848d1f1f52486e8e87d96af079aedfb1f
Howardleejhenhao <howard20051109@gmail.com>
2025-12-26 16:42:17 +0800

    fix: urls.py

--- webapp/config/urls.py
+++ webapp/config/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
+from django.urls import path
+
 from pages.views import home, score_conversion, standards_by_subject, category_compare, requirements
 from pages.views.category_compare import toggle_favorite
 from pages.views.favorites import favorites
